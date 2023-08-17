4deffa9c35f3c01a9966bda05e20a2789b16aadc
NicolasDekker <108103073+NicolasDekker@users.noreply.github.com>
2023-08-17 18:55:08 -0300

    arreglos container y algunos estilos de servicios

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -113,115 +113,118 @@ export default Formulario */
 const Formulario = () => {
     return (
         <form>
-            <div className=" bg-purple-900 p-4 md:p-8 lg:p-12 ">
-                <h2 className="text-white text-6xl mb-4 text-center font-bold pt-12 ">Formulario de contacto</h2>
-                <h6 className="text-white mb-4 text-center text-xl">Contáctese con nosotros y obtenga un asesoramiento personalizado</h6>
+            <div className='bg-purple-900'>
+                <div className="mx-auto p-4 bg-purple-900 container">
+                    <div className=" bg-purple-900 p-4 md:p-8 lg:p-12 ">
+                        <h2 className="text-white text-6xl mb-4 text-center font-bold pt-12 ">Formulario de contacto</h2>
+                        <h6 className="text-white mb-4 text-center text-xl">Contáctese con nosotros y obtenga un asesoramiento personalizado</h6>
 
-                <div className=" mb-4 grid grid-cols-1 place-items-center pt-8">
-                    <label
-                        className="text-white text-2xl pb-5"
-                        htmlFor="nombre"
-                    >
-                        Nombre
-                    </label>
-                    <div className="grid h-16 rounded-lg mt-2 bg-purple-700 grid-cols-12 gap-2 w-[350px] sm:w-[500px] md:w-[600px] lg:w-[940px] xl:w-[1200px] 2xl:w-[1400px] mx-auto">
-                        <span className="p-3 text-white col-span-1 grid place-content-center">
-                            <i className="mr-2 fas fa-font"></i>
-                        </span>
-                        <input
-                            id="nombre"
-                            type="text"
-                            className="p-3 bg-transparent w-full text-white grid col-span-11"
-                            placeholder="Escriba su nombre completo"
-                            name="nombre"
-                        />
-                    </div>
-                </div>
-                <div className="mb-4 grid grid-cols-1 place-items-center pt-8">
-                    <label
-                        className="text-white text-2xl pb-5"
-                        htmlFor="mail"
+                        <div className=" mb-4 grid grid-cols-1 place-items-center pt-8">
+                            <label
+                                className="text-white text-2xl pb-5"
+                                htmlFor="nombre"
+                            >
+                                Nombre
+                            </label>
+                            <div className="grid h-16 rounded-lg mt-2 bg-purple-700 grid-cols-12 gap-2 w-[350px] sm:w-[500px] md:w-[600px] lg:w-[940px] xl:w-[1200px] 2xl:w-[1400px] mx-auto">
+                                <span className="p-3 text-white col-span-1 grid place-content-center">
+                                    <i className="mr-2 fas fa-font"></i>
+                                </span>
+                                <input
+                                    id="nombre"
+                                    type="text"
+                                    className="p-3 bg-transparent w-full text-white grid col-span-11"
+                                    placeholder="Escriba su nombre completo"
+                                    name="nombre"
+                                />
+                            </div>
+                        </div>
+                        <div className="mb-4 grid grid-cols-1 place-items-center pt-8">
+                            <label
+                                className="text-white text-2xl pb-5"
+                                htmlFor="mail"
+                                
+                            >
+                                Correo electrónico
+                            </label>
+                            <div className="grid h-16 rounded-lg mt-2 bg-purple-700 grid-cols-12 gap-2 w-[350px] sm:w-[500px] md:w-[600px] lg:w-[940px] xl:w-[1200px] 2xl:w-[1400px] mx-auto">
+                                <span className="p-3 text-white col-span-1 grid place-content-center">
+                                    <i className="mr-2 fas fa-envelope"></i>
+                                </span>
+                                <input 
+                                    id="mail"
+                                    type="text"
+                                    className="p-3 bg-transparent w-full text-white grid col-span-11"
+                                    placeholder="Escriba un email donde desea obtener respuesta"
+                                    name="mail"
+                                />
+                            </div>
+                        </div>
+                        <div className="mb-4 grid grid-cols-1 place-items-center pt-8">
+                            <label
+                                className="text-white text-2xl pb-5"
+                                htmlFor="tel"
+                            >
+                                Número de contacto
+                            </label>
+                            <div className="grid h-16 rounded-lg mt-2 bg-purple-700 grid-cols-12 gap-2 w-[350px] sm:w-[500px] md:w-[600px] lg:w-[940px] xl:w-[1200px] 2xl:w-[1400px] mx-auto">
+                                <span className="p-3 text-white col-span-1 grid place-content-center">
+                                    <i className="mr-2 fas fa-phone"></i>
+                                </span>
+                                <input 
+                                    id="tel"
+                                    type="text"
+                                    className="p-3 bg-transparent w-full text-white grid col-span-11"
+                                    placeholder="Ej: (+54 11) 68126810"
+                                    name="tel"
+                                />
+                            </div>
+                        </div>
+                        <div className="mb-4 grid grid-cols-1 place-items-center pt-8">
+                            <label
+                                className="text-white text-2xl pb-5"
+                                htmlFor="servicio"   
+                            >
+                                Selecciona el servicio deseado
+                            </label>
+                            <div className="grid h-16 rounded-lg mt-2 bg-purple-700 grid-cols-12 gap-2 w-[350px] sm:w-[500px] md:w-[600px] lg:w-[940px] xl:w-[1200px] 2xl:w-[1400px] mx-auto">
+                                <select
+                                    id="servicio"
+                                    type="text"
+                                    className="p-3 bg-transparent w-full text-white grid col-span-12"
+                                    placeholder="Elija por el servicio que desea contactarnos"
+                                    name="servicio"
+                                >
+                                    <option className=" text-black" value="servicio_1">Servicio 1</option>
+                                    <option className=" text-black" value="servicio_2">Servicio 2</option>
+                                    <option className=" text-black" value="servicio_3">Servicio 3</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div className="mb-4 grid grid-cols-1 place-items-center pt-8 pb-40">
+                            <label
+                                className="text-white text-2xl pb-5"
+                                htmlFor="notas"
+                            >
+                                ¿Tienes alguna pregunta o solicitud especial?
+                            </label>
+                            <div className="grid place-content-center rounded-lg mt-2 bg-purple-700 grid-cols-12 gap-2 w-[350px] sm:w-[500px] md:w-[600px] lg:w-[940px] xl:w-[1200px] 2xl:w-[1400px] mx-auto h-32" >
+                                <textarea 
+                                    as="textarea"
+                                    id="notas"
+                                    type="text"
+                                    className="p-3 w-full h-full bg-transparent text-white grid col-span-12"
+                                    placeholder="Si lo desea agregue una pregunta a solicitud especial"
+                                    name="notas"
+                                />
+                            </div>
+                        </div>
                         
-                    >
-                        Correo electrónico
-                    </label>
-                    <div className="grid h-16 rounded-lg mt-2 bg-purple-700 grid-cols-12 gap-2 w-[350px] sm:w-[500px] md:w-[600px] lg:w-[940px] xl:w-[1200px] 2xl:w-[1400px] mx-auto">
-                        <span className="p-3 text-white col-span-1 grid place-content-center">
-                            <i className="mr-2 fas fa-envelope"></i>
-                        </span>
-                        <input 
-                            id="mail"
-                            type="text"
-                            className="p-3 bg-transparent w-full text-white grid col-span-11"
-                            placeholder="Escriba un email donde desea obtener respuesta"
-                            name="mail"
-                        />
                     </div>
                 </div>
-                <div className="mb-4 grid grid-cols-1 place-items-center pt-8">
-                    <label
-                        className="text-white text-2xl pb-5"
-                        htmlFor="tel"
-                    >
-                        Número de contacto
-                    </label>
-                    <div className="grid h-16 rounded-lg mt-2 bg-purple-700 grid-cols-12 gap-2 w-[350px] sm:w-[500px] md:w-[600px] lg:w-[940px] xl:w-[1200px] 2xl:w-[1400px] mx-auto">
-                        <span className="p-3 text-white col-span-1 grid place-content-center">
-                            <i className="mr-2 fas fa-phone"></i>
-                        </span>
-                        <input 
-                            id="tel"
-                            type="text"
-                            className="p-3 bg-transparent w-full text-white grid col-span-11"
-                            placeholder="Ej: (+54 11) 68126810"
-                            name="tel"
-                        />
-                    </div>
-                </div>
-                <div className="mb-4 grid grid-cols-1 place-items-center pt-8">
-                    <label
-                        className="text-white text-2xl pb-5"
-                        htmlFor="servicio"   
-                    >
-                        Selecciona el servicio deseado
-                    </label>
-                    <div className="grid h-16 rounded-lg mt-2 bg-purple-700 grid-cols-12 gap-2 w-[350px] sm:w-[500px] md:w-[600px] lg:w-[940px] xl:w-[1200px] 2xl:w-[1400px] mx-auto">
-                        <select
-                            id="servicio"
-                            type="text"
-                            className="p-3 bg-transparent w-full text-white grid col-span-12"
-                            placeholder="Elija por el servicio que desea contactarnos"
-                            name="servicio"
-                        >
-                            <option className=" text-black" value="servicio_1">Servicio 1</option>
-                            <option className=" text-black" value="servicio_2">Servicio 2</option>
-                            <option className=" text-black" value="servicio_3">Servicio 3</option>
-                        </select>
-                    </div>
-                </div>
-                <div className="mb-4 grid grid-cols-1 place-items-center pt-8 pb-40">
-                    <label
-                        className="text-white text-2xl pb-5"
-                        htmlFor="notas"
-                    >
-                        ¿Tienes alguna pregunta o solicitud especial?
-                    </label>
-                    <div className="grid place-content-center rounded-lg mt-2 bg-purple-700 grid-cols-12 gap-2 w-[350px] sm:w-[500px] md:w-[600px] lg:w-[940px] xl:w-[1200px] 2xl:w-[1400px] mx-auto h-32" >
-                        <textarea 
-                            as="textarea"
-                            id="notas"
-                            type="text"
-                            className="p-3 w-full h-full bg-transparent text-white grid col-span-12"
-                            placeholder="Si lo desea agregue una pregunta a solicitud especial"
-                            name="notas"
-                        />
-                    </div>
-                </div>
-                
             </div>
         </form>
     )
 }
 
 export default Formulario
-
--- src/components/Servicios.jsx
+++ src/components/Servicios.jsx
@@ -1,31 +1,45 @@
+import React, { useState } from 'react';
 import ImgPlanta from '/public/img/Frame-planta.png';
 import ImgServicios from '/public/img/servicio.jpg';
 
+
 const Servicios = () => {
+  const [showDesignText, setShowDesignText] = useState(false);
     return (
       <div className='bg-purple-900'>
       <div className="mx-auto p-4 bg-purple-900 container">
         
         <h1 className="text-4xl font-bold text-white bg-purple-900 text-center p-4 mb-4">SERVICIOS</h1>
 
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4"
+        onMouseEnter={() => setShowDesignText(true)}
+        onMouseLeave={() => setShowDesignText(false)}
+        >
           
           <div className="bg-purple-900 p-4 text-white text-center">
+            <div className="transition-transform duration-[2000ms] transform-gpu hover:rotate-[360deg]">
+              <iconify-icon icon="ph:pen-nib-light" width="70" style={{color:'#BA55D3'}}></iconify-icon>
+            </div>
             
-          <iconify-icon icon="ph:pen-nib-light" width="70" style={{color:'#BA55D3'}}></iconify-icon>
             <h2 className="text-xl font-bold mt-8">Diseño Web</h2>
-            <p className="text-justify mt-10">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore quae maxime molestiae eaque minus architecto, necessitatibus omnis porro hic enim sequi inventore asperiores, voluptas voluptates placeat sit aspernatur doloribus expedita.</p>
+            <div>
+              {showDesignText && (
+                <p className="text-justify mt-10">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore quae maxime molestiae eaque minus architecto, necessitatibus omnis porro hic enim sequi inventore asperiores, voluptas voluptates placeat sit aspernatur doloribus expedita.</p>
+              )}
+            </div>
           </div>
           <div className="bg-purple-900 p-4 text-white text-center">
-            
-          <iconify-icon icon="emojione-monotone:page-facing-up" width="70" style={{color:'#BA55D3'}}></iconify-icon>
+          <div className="transition-transform duration-[2000ms] transform-gpu hover:rotate-[360deg]">
+            <iconify-icon icon="emojione-monotone:page-facing-up" width="70" style={{color:'#BA55D3'}}></iconify-icon>
+          </div>
             <h2 className="text-xl text-center font-bold mt-8">Desarrollo</h2>
             <p className="text-justify mt-10">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore quae maxime molestiae eaque minus architecto, necessitatibus omnis porro hic enim sequi inventore asperiores, voluptas voluptates placeat sit aspernatur doloribus expedita.</p>
           </div>
 
           <div className="bg-purple-900 p-4 text-white text-center">
-
-          <iconify-icon icon="iconoir:wrench" width="70" style={{color:'#BA55D3'}}></iconify-icon>
+          <div className="transition-transform duration-[2000ms] transform-gpu hover:rotate-[360deg]">
+            <iconify-icon icon="iconoir:wrench" width="70" style={{color:'#BA55D3'}}></iconify-icon>
+          </div>
             <h2 className="text-xl text-center font-bold mt-8">Mantenimiento</h2>
             <p className="text-justify mt-10">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore quae maxime molestiae eaque minus architecto, necessitatibus omnis porro hic enim sequi inventore asperiores, voluptas voluptates placeat sit aspernatur doloribus expedita.</p>
           </div>
