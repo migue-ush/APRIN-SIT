b6c544a745ef28d1957e140cdd3004602b0cc7e9
JRenaldi <jeros_lp@hotmail.com>
2023-08-24 14:25:14 -0300

    Funcion resulta burger button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,52 @@
-import React from 'react'
+import { useState } from 'react'
+import { Menu, X } from 'lucide-react'
+
+
+
+const NavLinks = () => {
+    return (
+        <>
+            <ul className="ml-5 pl-5 underline-offset-8 md:pt-12 w-[60%] md:w-[100%] md:pl-0 bg-purple-900 opacity-70 rounded-xl md:bg-transparent gap-x-14 xl:text-xl lg:text-lg md:text-sm md:grid md:grid-cols-4 ">
+                <li className='text-lg font-bold pb-5 text-center pt-5  md:text-base md:font-normal md:pb-0'><a href="/" className="text-white md:col-span-1 ">Home</a></li>
+                <li className='text-lg font-bold pb-5 text-center md:text-base md:font-normal md:pb-0'><a href="#Nosotros" className="text-white md:col-span-1 ">Nosotros</a></li>
+                <li className='text-lg font-bold pb-5 text-center md:text-base md:font-normal md:pb-0'><a href="#Servicios" className="text-white md:col-span-1">Servicios</a></li>
+                <li className='text-lg font-bold pb-5 text-center md:text-base md:font-normal md:pb-0'><a href="#Formulario" className="text-white md:col-span-1">Contactos</a></li>
+            </ul>
+        </>
+    )
+}
 
 const Navbar = () => {
+
+    const [isOpen, setIsOpen] = useState(false)
+
+    const toggleNavbar = () => {
+        setIsOpen(!isOpen)
+    }
+
 return (
-    <nav className=" opacity-100 gap-3 grid  md:grid-cols-3  ">
-        <ul className="pt-12 pl-5 gap-x-14 hidden xl:text-xl lg:text-lg md:text-sm md:grid md:grid-cols-4 ">
-            <li className=''><a href="/" className="text-white md:col-span-1 ">Home</a></li>
-            <li className=''><a href="#Nosotros" className="text-white md:col-span-1 ">Nosotros</a></li>
-            <li className=''><a href="#Servicios" className="text-white md:col-span-1">Servicios</a></li>
-            <li className=''><a href="#Formulario" className="text-white md:col-span-1">Contactos</a></li>
-        </ul>
-        <a href="/"><img src="../img/Identidad_Aprin.png" alt="" className='w-[160px] h-[112px] mx-auto ' /></a>
-        <ul className=" hidden space-x-4 pt-10 mx-auto md:grid md:grid-cols-2 text-3xl">
-            <li><a href="https://www.instagram.com/aprin_sit/"target='_blank'><i className="fa-brands fa-instagram text-white" ></i></a></li>
-            <li><a href="https://www.facebook.com/profile.php?id=100095344063526" target='_blank'><i className="fa-brands fa-facebook text-white h-5 w-5"></i></a></li>
-        </ul>
-        <div className="space-y-2 grid pt-14 pl-5 sm:pl-10 absolute md:hidden">
-            <div className="w-8 h-0.5 bg-gray-100"></div>
-            <div className="w-8 h-0.5 bg-gray-100"></div>
-            <div className="w-8 h-0.5 bg-gray-100"></div>
-        </div>
-    </nav>
+    <>
+        <nav className=" opacity-100 gap-3 grid  md:grid-cols-3  ">
+            <div className='hidden md:grid'>
+                <NavLinks/>
+            </div>
+            <a href="/"><img src="../img/Identidad_Aprin.png" alt="" className='w-[160px] h-[112px] mx-auto ' /></a>
+            <ul className=" hidden space-x-4 pt-10 mx-auto md:grid md:grid-cols-2 text-3xl">
+                <li><a href="https://www.instagram.com/aprin_sit/"target='_blank'><i className="fa-brands fa-instagram text-white" ></i></a></li>
+                <li><a href="https://www.facebook.com/profile.php?id=100095344063526" target='_blank'><i className="fa-brands fa-facebook text-white h-5 w-5"></i></a></li>
+            </ul>
+            <div className="space-y-2 grid pt-14 pl-5 sm:pl-10 absolute md:hidden">
+                <button onClick={toggleNavbar}>
+                    {isOpen ? <X color="red"/> : <Menu color='white'/>}
+                </button>
+            </div>
+        </nav>
+        {isOpen && (
+            <div>
+                <NavLinks/>
+            </div>
+        )}
+    </>
 )
 }
 
