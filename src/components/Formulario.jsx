f8a49accc683e2218894942aabf900f34efc0a85
migue-ush <albornozmiguel@gmail.com>
2023-09-05 19:20:16 -0300

    Siguiendo con el formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -18,8 +18,8 @@ const Formulario = () => {
     } else if (form.current.nombre_usuario.value.length < 5) {
       errors.nombre_usuario = "Mínimo 5 caracteres";
     } else if (!/^[\w@]+$/.test(form.current.nombre_usuario.value)) {
-      errors.nombre_usuario = "Solo caracteres válidos";      
-    } 
+      errors.nombre_usuario = "Solo caracteres válidos";
+    }
 
     if (!form.current.email_usuario.value) {
       errors.email_usuario = "El correo electrónico es requerido";
@@ -41,7 +41,7 @@ const Formulario = () => {
       errors.notas = "Mínimo 20 caracteres";
     } else if (form.current.notas.value.length > 200) {
       errors.notas = "Máximo 200 caracteres";
-    }else if (!form.current.notas.value) {
+    } else if (!form.current.notas.value) {
       errors.notas = "Utilice solo letras y números";
     }
 
@@ -86,7 +86,7 @@ const Formulario = () => {
   return (
     <form ref={form} onSubmit={sendEmail} id="Formulario" className="h-full font-mont">
       <div className="mb-4 bg-purple-950">
-        <div className="mx-auto p-4 bg-purple-950 container">
+        <div className="mx-auto p-2 bg-purple-950 container">
           <div className=" bg-purple-950 md:p-8 lg:p-12 ">
             <h2 className="text-white text-3xl uppercase lg:text-4xl 2xl:text-5xl mb-4 text-center font-bold pt-12">
               Formulario de contacto
@@ -94,145 +94,148 @@ const Formulario = () => {
             <h6 className="text-white mb-4 text-center text-xl">
               Contactate con nosotros y obtené un asesoramiento personalizado
             </h6>
-            <div className="mb-4 grid place-items-center grid-cols-1 pt-8">
-              <label className="text-white text-2xl pb-5" htmlFor="nombre_usuario">
-                Nombre y Apellido
-              </label>
-              <div className="grid h-16 rounded-lg mt-2 bg-purple-800 grid-cols-12 gap-2 w-[350px] sm:w-[500px] md:w-[600px] lg:w-[940px] xl:w-[1200px] mx-auto">
-                <span className="p-3 text-white col-span-1 grid place-content-center">
-                  <i className="ml-1 fas fa-font"></i>
-                </span>
-                <input
-                  id="nombre_usuario"
-                  type="text"
-                  className="p-3 bg-transparent w-full text-white grid col-span-11"
-                  placeholder="Escribí tu nombre"
-                  name="nombre_usuario"
-                />
-              </div>
-                {validationErrors.nombre_usuario && (
-                  <div className="text-red-500 font-bold">
-                    {validationErrors.nombre_usuario}
-                  </div>
-                )}
+          </div>
+        </div>
+        <div className="mx-auto p-2 bg-purple-950 container">
+          <div className="mb-4 grid place-items-center grid-cols-1">
+            <label className="text-white text-2xl pb-5" htmlFor="nombre_usuario">
+              Nombre y Apellido
+            </label>
+            <div className="grid h-16 rounded-lg mt-2 bg-purple-800 grid-cols-12 gap-2 w-[350px] sm:w-[500px] md:w-[600px] lg:w-[940px] xl:w-[1200px] mx-auto">
+              <span className="p-3 text-white col-span-1 grid place-content-center">
+                <i className="ml-1 fas fa-font"></i>
+              </span>
+              <input
+                id="nombre_usuario"
+                type="text"
+                className="p-3 bg-transparent w-full text-white grid col-span-11"
+                placeholder="Escribí tu nombre"
+                name="nombre_usuario"
+              />
             </div>
-            <div className="mb-4 place-items-center grid grid-cols-1 pt-8">
-              <label className="text-white text-2xl pb-5" htmlFor="email_usuario">
-                Correo electrónico
-              </label>
-              <div className="grid h-16 rounded-lg mt-2 bg-purple-800 grid-cols-12 gap-2 w-[350px] sm:w-[500px] md:w-[600px] lg:w-[940px] xl:w-[1200px] mx-auto">
-                <span className="p-3 text-white col-span-1 grid place-content-center">
-                  <i className="ml-1 fas fa-envelope"></i>
-                </span>
-                <input
-                  id="email_usuario"
-                  type="email"
-                  className="p-3 bg-transparent w-full text-white grid col-span-11"
-                  placeholder="Escribí tu email"
-                  name="email_usuario"                  
-                />
+            {validationErrors.nombre_usuario && (
+              <div className="text-red-500 font-bold">
+                {validationErrors.nombre_usuario}
               </div>
-                {validationErrors.email_usuario && (
-                  <div className="text-red-500 font-bold">
-                    {validationErrors.email_usuario}
-                  </div>
-                )}
+            )}
+          </div>
+          <div className="mb-4 place-items-center grid grid-cols-1 pt-8">
+            <label className="text-white text-2xl pb-5" htmlFor="email_usuario">
+              Correo electrónico
+            </label>
+            <div className="grid h-16 rounded-lg mt-2 bg-purple-800 grid-cols-12 gap-2 w-[350px] sm:w-[500px] md:w-[600px] lg:w-[940px] xl:w-[1200px] mx-auto">
+              <span className="p-3 text-white col-span-1 grid place-content-center">
+                <i className="ml-1 fas fa-envelope"></i>
+              </span>
+              <input
+                id="email_usuario"
+                type="email"
+                className="p-3 bg-transparent w-full text-white grid col-span-11"
+                placeholder="Escribí tu email"
+                name="email_usuario"
+              />
             </div>
-            <div className="mb-4 place-items-center grid grid-cols-1 pt-8">
-              <label className="text-white text-2xl pb-5 " htmlFor="tel_usuario">
-                Número de contacto
-              </label>
-              <div className="grid h-16 rounded-lg mt-2 bg-purple-800 grid-cols-12 gap-2 w-[350px] sm:w-[500px] md:w-[600px] lg:w-[940px] xl:w-[1200px] mx-auto">
-                <span className="flex items-center p-3 text-white">
-                  <i className="ml-1 fas fa-phone"></i>
-                </span>
-                <input
-                  id="tel_usuario"
-                  type="text"
-                  className="p-3 bg-transparent w-full text-white grid col-span-11 "
-                  placeholder="Ej: (+54 11) 68126810"
-                  name="tel_usuario"
-                />
+            {validationErrors.email_usuario && (
+              <div className="text-red-500 font-bold">
+                {validationErrors.email_usuario}
               </div>
-                {validationErrors.tel_usuario && (
-                  <div className="text-red-500 font-bold">
-                    {validationErrors.tel_usuario}
-                  </div>
-                )}
+            )}
+          </div>
+          <div className="mb-4 place-items-center grid grid-cols-1 pt-8">
+            <label className="text-white text-2xl pb-5 " htmlFor="tel_usuario">
+              Número de contacto
+            </label>
+            <div className="grid h-16 rounded-lg mt-2 bg-purple-800 grid-cols-12 gap-2 w-[350px] sm:w-[500px] md:w-[600px] lg:w-[940px] xl:w-[1200px] mx-auto">
+              <span className="flex items-center p-3 text-white">
+                <i className="ml-1 fas fa-phone"></i>
+              </span>
+              <input
+                id="tel_usuario"
+                type="text"
+                className="p-3 bg-transparent w-full text-white grid col-span-11 "
+                placeholder="Ej: (+54 11) 68126810"
+                name="tel_usuario"
+              />
             </div>
-            <div className="mb-4 place-items-center grid grid-cols-1 pt-8">
-              <label className="text-white text-2xl pb-5" htmlFor="servicio">
-                Seleccioná el servicio deseado
-              </label>
-              <div className="grid h-16 rounded-lg mt-2 bg-purple-800 grid-cols-12 gap-2 w-[350px] sm:w-[500px] md:w-[600px] lg:w-[940px] xl:w-[1200px] mx-auto">
-                <select
-                  id="servicio"
-                  className={`p-3 bg-transparent w-full text-white grid col-span-12 ${
-                    validationErrors.servicio ? "border-red-500" : ""
+            {validationErrors.tel_usuario && (
+              <div className="text-red-500 font-bold">
+                {validationErrors.tel_usuario}
+              </div>
+            )}
+          </div>
+          <div className="mb-4 place-items-center grid grid-cols-1 pt-8">
+            <label className="text-white text-2xl pb-5" htmlFor="servicio">
+              Seleccioná el servicio deseado
+            </label>
+            <div className="grid h-16 rounded-lg mt-2 bg-purple-800 grid-cols-12 gap-2 w-[350px] sm:w-[500px] md:w-[600px] lg:w-[940px] xl:w-[1200px] mx-auto">
+              <select
+                id="servicio"
+                className={`p-3 bg-transparent w-full text-white grid col-span-12 ${validationErrors.servicio ? "border-red-500" : ""
                   }`}
-                  name="servicio"
-                  onChange={(e) => {
-                    form.current.servicio.value = e.target.value;
-                    setValidationErrors({
-                      ...validationErrors,
-                      servicio: "",
-                    });
-                  }}
-                >
-                  <option className="text-black" value="">
-                    Seleccioná un servicio
-                  </option>
-                  <option className="text-black" value="disenio_web">
-                    Diseño web
-                  </option>
-                  <option className="text-black" value="desarrollo">
-                    Desarrollo
-                  </option>
-                  <option className="text-black" value="mantenimiento">
-                    Mantenimiento
-                  </option>
-                  <option className="text-black" value="otros">
-                    Otros
-                  </option>
-                </select>
-              </div>
-              {validationErrors.servicio && (
-                <div className="text-red-500 font-bold">{validationErrors.servicio}</div>
-              )}
-            </div>
-            <div className="mb-4 grid grid-cols-1 place-items-center pt-8 pb-10">
-              <label className="text-white text-2xl pb-5" htmlFor="notas">
-                ¿Tenés alguna pregunta o solicitud especial?
-              </label>
-              <div className="grid place-content-center rounded-lg mt-2 bg-purple-800 grid-cols-12 gap-2 w-[350px] sm:w-[500px] md:w-[600px] lg:w-[940px] xl:w-[1200px]  mx-auto h-32">
-                <textarea
-                  as="textarea"
-                  id="notas"
-                  type="text"
-                  className="p-3 w-full h-32 bg-transparent text-white grid col-span-12 resize-none"
-                  placeholder="Escribí tu pregunta o solicitud"
-                  name="notas"
-                />
-              </div>
-                {validationErrors.notas && (
-                  <div className="text-red-500 font-bold">{validationErrors.notas}</div>
-                  )}
-            </div>
-            <div className="grid place-items-center py-5 ">
-              <button                
-                type="submit"
-                className="w-44 h-16 lg:w-[300px] lg:h-20 bg-purple-700 hover:bg-purple-400 hover:text-violet-600 text-white font-bold py-4 px-8 border-b-4 border-purple-700 hover:border-purple-500 rounded-lg text-xl"
+                name="servicio"
+                onChange={(e) => {
+                  form.current.servicio.value = e.target.value;
+                  setValidationErrors({
+                    ...validationErrors,
+                    servicio: "",
+                  });
+                }}
               >
-                ENVIAR
-              </button>
+                <option className="text-black" value="">
+                  Seleccioná un servicio
+                </option>
+                <option className="text-black" value="disenio_web">
+                  Diseño web
+                </option>
+                <option className="text-black" value="desarrollo">
+                  Desarrollo
+                </option>
+                <option className="text-black" value="mantenimiento">
+                  Mantenimiento
+                </option>
+                <option className="text-black" value="otros">
+                  Otros
+                </option>
+              </select>
             </div>
+            {validationErrors.servicio && (
+              <div className="text-red-500 font-bold">{validationErrors.servicio}</div>
+            )}
+          </div>
+          <div className="mb-4 grid grid-cols-1 place-items-center pt-8 pb-10">
+            <label className="text-white text-2xl pb-5" htmlFor="notas">
+              ¿Tenés alguna pregunta o solicitud especial?
+            </label>
+            <div className="grid place-content-center rounded-lg mt-2 bg-purple-800 grid-cols-12 gap-2 w-[350px] sm:w-[500px] md:w-[600px] lg:w-[940px] xl:w-[1200px]  mx-auto h-32">
+              <textarea
+                as="textarea"
+                id="notas"
+                type="text"
+                className="p-3 w-full h-32 bg-transparent text-white grid col-span-12 resize-none"
+                placeholder="Escribí tu pregunta o solicitud"
+                name="notas"
+              />
+            </div>
+            {validationErrors.notas && (
+              <div className="text-red-500 font-bold">{validationErrors.notas}</div>
+            )}
           </div>
         </div>
       </div>
+      <div className="grid place-items-center py-5 ">
+        <button
+          type="submit"
+          className="w-44 h-16 lg:w-[300px] lg:h-20 bg-purple-700 hover:bg-purple-400 hover:text-violet-600 text-white font-bold py-4 px-8 border-b-4 border-purple-700 hover:border-purple-500 rounded-lg text-xl"
+        >
+          ENVIAR
+        </button>
+      </div>
+
+
       <a href="https://wa.me/5491154555573" target="_blank" rel="noopener noreferrer">
         <img src="../img/whatsapp_logo.svg" loading="lazy" alt="WhatsApp Logo" className="fixed bottom-12 right-12 z-50 w-16 h-auto"></img>
       </a>
-    </form>
+    </form >
   );
 };
 
