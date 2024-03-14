e632deccc1abf71048b1994243076b904e051bdf
fessimon <fessimon@gmail.com>
2024-03-13 22:50:37 -0300

    car 2.0: actualizacion de forma de mostrar las imagenes

--- src/components/Carrousel.jsx
+++ src/components/Carrousel.jsx
@@ -3,6 +3,12 @@ import React, { useEffect } from 'react'
 import imgCar from "../data/imagenesCar.json"
 import { swiffyslider } from 'swiffy-slider';
 import "swiffy-slider/css";
+import { lazy } from 'react';
+window.swiffyslider = swiffyslider;
+
+window.addEventListener("load", () => {
+    window.swiffyslider.init();
+});
 
 
 
@@ -10,39 +16,71 @@ const Carrousel = () => {
 
     const imagenesLogos = imgCar.imagenesLogoCar;
 
-    useEffect(() => {
-        if (typeof window !== 'undefined') {
-            window.swiffyslider = swiffyslider;
-            window.swiffyslider.init()
-        }
-    }, []);
-
     return (
-        <div className="swiffy-slider slider-item-show4 slider-nav-outside slider-nav-dark slider-nav-sm slider-nav-visible slider-nav-page slider-item-snapstart slider-nav-autoplay slider-nav-autopause slider-item-ratio slider-item-ratio-contain slider-item-ratio-32x9 bg-white shadow-lg py-3 py-lg-4" data-slider-nav-autoplay-interval="2000">
+        <div className="
+                        swiffy-slider 
+                        slider-item-show4 
+                        slider-nav-outside 
+                        slider-nav-dark 
+                        slider-nav-sm 
+                        slider-nav-visible 
+                        slider-nav-page 
+                        slider-item-snapstart 
+                        slider-nav-autoplay 
+                        slider-nav-autopause 
+                        slider-item-ratio 
+                        slider-item-ratio-contain 
+                        slider-item-ratio-32x9
+                        bg-[#6b4996] shadow-lg py-3 py-lg-4" data-slider-nav-autoplay-interval="1500">
             <div className="slider-container">
+                {
+                    imagenesLogos.map((imgLogo, index)=>{
+                        <div key={index}>
+                            <Image 
+                                src={imgLogo.imgSrc} 
+                                alt={imgLogo.alt}
+                                width={450}
+                                height={150}
+                                loading={lazy}
+                             />
+                        </div>
+
+                    })
+                }
+
+
                 <div>
-                    <Image 
-                        src={"../img/logos/nextjs_icon_dark.svg"}
-                        alt="..." />
+                    <img src='../img/logos/nextjs_icon_dark.svg' alt="..." />
                 </div>
                 <div>
-                    <Image 
-                        src={"../img/logos/nextjs_icon_dark.svg"}
-                        loading="lazy" 
-                        alt="..." />
+                    <img src='../img/logos/nodejs.svg' alt="..." />
                 </div>
                 <div>
-                    <Image 
-                        src={"../img/logos/nextjs_icon_dark.svg"}
-                        loading="lazy" 
-                        alt="..." />
+                    <img src='../img/logos/nextjs_icon_dark.svg' alt="..." />
                 </div>
                 <div>
-                    <Image 
-                        src={"../img/logos/nextjs_icon_dark.svg"}
-                        loading="lazy" 
-                        alt="..." />
+                    <img src='../img/logos/react.svg' alt="..." />
                 </div>
+                <div>
+                    <img src='../img/logos/tailwindcss.svg' alt="..." />
+                </div>
+                <div>
+                    <img src='../img/logos/nextjs_icon_dark.svg' alt="..." />
+                </div>
+                <div>
+                    <img src='../img/logos/nodejs.svg' alt="..." />
+                </div>
+                <div>
+                    <img src='../img/logos/nextjs_icon_dark.svg' alt="..." />
+                </div>
+                <div>
+                    <img src='../img/logos/react.svg' alt="..." />
+                </div>
+                <div>
+                    <img src='../img/logos/tailwindcss.svg' alt="..." />
+                </div>
+                
+                
             </div>
 
             <button type="button" className="slider-nav" aria-label="Go left"></button>
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,6 @@ export default {
       'sour': ['Source Code Pro']
 
     }
-
   },
   plugins: [],
 }
