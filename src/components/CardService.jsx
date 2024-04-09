be09733575150251a1f202b7edd277f9b762160c
sebaF90 <sebastianfreiberger03@gmail.com>
2024-04-09 18:45:10 -0300

    agrego animaciones a cada card de servicios

--- src/components/CardService.jsx
+++ src/components/CardService.jsx
@@ -1,55 +1,68 @@
-import React from "react";
+import React, { useState, MouseEvent, useCallback } from 'react';
 
-const servicesData = [
-  {
-    icon: "ph:pen-nib-light",
-    title: "Diseño Web",
-    description:
-      "Diseñamos la apariencia visual y la experiencia del usuario en un sitio web o aplicación. Utilizamos una variedad de herramientas y técnicas para crear diseños atractivos y funcionales que mejoren la experiencia del usuario, refuercen la marca y la identidad visual de una empresa o individuo.",
-    image:"/img/Web-Designing-Important-for-Your-Business.jpg",
-  },
-  {
-    icon: "emojione-monotone:page-facing-up",
-    title: "Desarrollo",
-    description:
-      "Creamos sitios web simples con contenido estático hasta el desarrollo de aplicaciones web complejas con funcionalidades avanzadas. Este servicio es esencial para cualquier empresa o individuo que desee tener una presencia en línea efectiva y profesional.",
-      image:"/img/iStock-629285904-e1515424561997.jpg",
-  },
-  {
-    icon: "iconoir:wrench",
-    title: "Mantenimiento",
-    description:
-      "Actualizamos y mantenemos de forma regular tu sitio web o aplicación para garantizar su correcto funcionamiento y seguridad. Mejoramos el rendimiento e implementamos medidas de seguridad para mantener tu sitio en óptimas condiciones.",
-      image:"/img/computer-repair-service.jpg",
-  },
-];
+const CardService = ({ icon, title, description, image }) => {
+  const [rotate, setRotate] = useState({ x: 0, y: 0 });
 
-const CardService = () => {
+  const onMouseMove = useCallback(
+    throttle((e) => {
+      const card = e.currentTarget;
+      const box = card.getBoundingClientRect();
+      const x = e.clientX - box.left;
+      const y = e.clientY - box.top;
+      const centerX = box.width / 2;
+      const centerY = box.height / 2;
+      const rotateX = (y - centerY) / 10; // Ajuste aquí para la sensibilidad de la inclinación en el eje X
+      const rotateY = (centerX - x) / 10; // Ajuste aquí para la sensibilidad de la inclinación en el eje Y
+
+      setRotate({ x: rotateX, y: rotateY });
+    }, 100),
+    []
+  );
+
+  const onMouseLeave = () => {
+    setRotate({ x: 0, y: 0 });
+  };
+
+  function throttle(func, delay) {
+    let lastCall = 0;
+    return (...args) => {
+      const now = new Date().getTime();
+      if (now - lastCall < delay) {
+        return;
+      }
+      lastCall = now;
+      return func(...args);
+    };
+  }
+
   return (
-    <>
-      {servicesData.map((service, index) => (
-        <div key={index} className="bg-purple-900 p-4 2xl:w-3/4 2xl:p-14 2xl:m-auto 2xl:h-[55vh] border-purple-400 border-2 rounded-2xl text-white text-center">
-          <div className="absolute">
-            {/* <img className="relative bottom-14 rounded-t-2xl right-14" src={service.image} width={364} alt=""/> */}
-          </div>
-          <div>
-            <div className="">
-              <iconify-icon
-                icon={service.icon}
-                width="70"
-                style={{ color: "#ffffff" }}
-              ></iconify-icon>
-            </div>
-            <h2 className="text-2xl font-bold mt-8 ">{service.title}</h2>
-            <div>
-              <p className="mt-10 ">{service.description}</p>
-            </div>
-          </div>
+    <div
+      className="bg-purple-900 p-4 border-purple-400 border-2 rounded-2xl text-white text-center relative"
+      onMouseMove={onMouseMove}
+      onMouseLeave={onMouseLeave}
+      style={{
+        transform: `perspective(1000px) rotateX(${rotate.x}deg) rotateY(${rotate.y}deg) scale3d(1, 1, 1)`,
+        transition: 'all 400ms cubic-bezier(0.25, 0.1, 0.25, 1) 0s', // Ajuste aquí para la suavidad de la transición
+      }}
+    >
+      <div className="absolute">
+        {/* <img className="relative bottom-14 rounded-t-2xl right-14" src={image} width={364} alt=""/> */}
+      </div>
+      <div>
+        <div className="">
+          <iconify-icon
+            icon={icon}
+            width="70"
+            style={{ color: "#ffffff" }}
+          ></iconify-icon>
         </div>
-      ))}
-    </>
+        <h2 className="text-2xl font-bold mt-8 ">{title}</h2>
+        <div>
+          <p className="mt-10 ">{description}</p>
+        </div>
+      </div>
+    </div>
   );
 };
-
 
 export default CardService;
--- src/components/Servicios.jsx
+++ src/components/Servicios.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import CardService from "./CardService";
-import ImgQwertty from "/img/qwertty.png";
+import React from 'react';
+import CardService from './CardService';
+import ImgQwertty from '/img/qwertty.png';
 
-const Servicios = () => {  
+const Servicios = () => {
   return (
     <div className="bg-purple-950 font-mont pb-60" id="Servicios">
       <div
@@ -14,7 +14,24 @@ const Servicios = () => {
           SERVICIOS
         </h1>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-          <CardService />
+          <CardService
+            icon="ph:pen-nib-light"
+            title="Diseño Web"
+            description="Diseñamos la apariencia visual y la experiencia del usuario en un sitio web o aplicación. Utilizamos una variedad de herramientas y técnicas para crear diseños atractivos y funcionales que mejoren la experiencia del usuario, refuercen la marca y la identidad visual de una empresa o individuo."
+            image="/img/Web-Designing-Important-for-Your-Business.jpg"
+          />
+          <CardService
+            icon="emojione-monotone:page-facing-up"
+            title="Desarrollo"
+            description="Creamos sitios web simples con contenido estático hasta el desarrollo de aplicaciones web complejas con funcionalidades avanzadas. Este servicio es esencial para cualquier empresa o individuo que desee tener una presencia en línea efectiva y profesional."
+            image="/img/iStock-629285904-e1515424561997.jpg"
+          />
+          <CardService
+            icon="iconoir:wrench"
+            title="Mantenimiento"
+            description="Actualizamos y mantenemos de forma regular tu sitio web o aplicación para garantizar su correcto funcionamiento y seguridad. Mejoramos el rendimiento e implementamos medidas de seguridad para mantener tu sitio en óptimas condiciones."
+            image="/img/computer-repair-service.jpg"
+          />
         </div>
       </div>
     </div>
