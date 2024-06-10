753d122bf418754ff6daebedc61aeac15e601768
migue-ush <albornozmiguel@gmail.com>
2024-06-10 13:00:00 -0300

    Implementación de mask-image con una clase personalizada de Tailwind CSS.

--- src/components/SobreNosotros.jsx
+++ src/components/SobreNosotros.jsx
@@ -7,13 +7,14 @@ const SobreNosotros = () => {
             <div className='w-[90%] container mx-auto'> 
                 <section className='section-about w-full bg-purple-950 grid grid-cols-4 mt-10'>
                     <div className='grid md:col-span-2 place-items-center col-span-4'>
-                        <img src={ImgSecond}  alt="desarrollo-web-a-medida-4" className='w-[90%] '/>
+                        <img src={ImgSecond} alt="desarrollo-web-a-medida-4" className='w-[90%] masked-image'/>
                     </div>
                     <div className='w-[95%] grid col-span-4 place-items-center p-5 md:col-span-2'>
                         <h1 className='text-3xl lg:text-4xl 2xl:text-5xl text-white w-[100%] font-bold my-5 text-center'>SOBRE NOSOTROS</h1>
                         <p className='text-white text-lg w-[100%] text-left'>
                             Somos una empresa joven de soluciones IT que integra consultoría en informática, desarrollo de sistemas de información, diseño de soluciones, servicio de soporte técnico y asesoramiento profesional.<br /><br />
-                            Estamos en búsqueda de socios tecnológicos con quienes podamos colaborar para potenciar sus negocios a través de la innovación tecnológica y una asociación estratégica que beneficie a ambas partes. Nuestro objetivo es combinar nuestros conocimientos y recursos para impulsar conjuntamente el crecimiento y el éxito en el ámbito tecnológico.</p>        
+                            Estamos en búsqueda de socios tecnológicos con quienes podamos colaborar para potenciar sus negocios a través de la innovación tecnológica y una asociación estratégica que beneficie a ambas partes. Nuestro objetivo es combinar nuestros conocimientos y recursos para impulsar conjuntamente el crecimiento y el éxito en el ámbito tecnológico.
+                        </p>        
                     </div>
                 </section>
             </div>
@@ -21,4 +22,4 @@ const SobreNosotros = () => {
     )
 }
 
-export default SobreNosotros
+export default SobreNosotros;
